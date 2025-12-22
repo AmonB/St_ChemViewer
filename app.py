12c11bf5fd4bf9ec9665e3e2fb4f5f564872b96a
AmonB <64903539+AmonB@users.noreply.github.com>
2025-12-22 21:29:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-home_page = st.Page("home_page.py", title="Home", icon="🏠")
+home_page = st.Page("pages/home_page.py", title="Home", icon="🏠")
 analysis_page = st.Page("pages/gaussian.py", title="Analysis", icon="🔍")
 file_browser_page = st.Page("pages/file_browser.py", title="File Browser", icon="📁")
 view_file_page = st.Page("pages/file_reader.py", title="File Reader", icon="📄️")
@@ -8,3 +8,4 @@ view_file_page = st.Page("pages/file_reader.py", title="File Reader", icon="📄
 pg = st.navigation([home_page, analysis_page, file_browser_page, view_file_page])
 
 pg.run()
+
